91caed4e96f7efefeaab84bc31a680e7880d5772
Tarik Amehri <amehritarik@gmail.com>
2025-05-09 11:11:50 +0100

    migrated everything into a single file

--- client/data/src/index.ts
+++ client/data/src/index.ts
@@ -1,47 +1,45 @@
-import Message from './protocole/shared/Message.js';
-import WS from './protocole/ws-client.js';
-import WSError from './protocole/shared/WSError.js';
+import WS, { Message, Connect, Invite, Play, Hook, WSError } from './protocole/ws-client.js';
 
 // ! Connect -----------------------------------------------------------
 const connect: string = WS.ConnectMessage('tarikkudesu');
 console.log(connect);
-const connectJson: Message = WS.Json({ message: connect, target: WS.Message.instance });
+const connectJson: Message = WS.Json({ message: connect, target: Message.instance });
 console.log(connectJson);
-const connectData = WS.Json({ message: connectJson.data, target: WS.Connect.instance });
+const connectData = WS.Json({ message: connectJson.data, target: Connect.instance });
 console.log(connectData);
 console.log('--------------------------------------------------------');
 
 // ! Invite -----------------------------------------------------------
 const invite: string = WS.InviteMessage('tarikkudesu', 'kisama');
 console.log(invite);
-const inviteJson: Message = WS.Json({ message: invite, target: WS.Message.instance });
+const inviteJson: Message = WS.Json({ message: invite, target: Message.instance });
 console.log(inviteJson);
-const inviteData = WS.Json({ message: inviteJson.data, target: WS.Invite.instance });
+const inviteData = WS.Json({ message: inviteJson.data, target: Invite.instance });
 console.log(inviteData);
 console.log('--------------------------------------------------------');
 
 // ! Play -----------------------------------------------------------
 const play: string = WS.PlayMessage('tarikkudesu', 'kisama');
 console.log(play);
-const playJson: Message = WS.Json({ message: play, target: WS.Message.instance });
+const playJson: Message = WS.Json({ message: play, target: Message.instance });
 console.log(playJson);
-const playData = WS.Json({ message: playJson.data, target: WS.Play.instance });
+const playData = WS.Json({ message: playJson.data, target: Play.instance });
 console.log(playData);
 console.log('--------------------------------------------------------');
 
 // ! Hook -----------------------------------------------------------
 const hook: string = WS.HookMessage(true, false);
 console.log(hook);
-const hookJson: Message = WS.Json({ message: hook, target: WS.Message.instance });
+const hookJson: Message = WS.Json({ message: hook, target: Message.instance });
 console.log(hookJson);
-const hookData = WS.Json({ message: hookJson.data, target: WS.Hook.instance });
+const hookData = WS.Json({ message: hookJson.data, target: Hook.instance });
 console.log(hookData);
 console.log('--------------------------------------------------------');
 
 // ! Error -----------------------------------------------------------
 const error: string = WS.ErrorMessage('not an error');
-const errorJson: Message = WS.Json({ message: error, target: WS.Message.instance });
+const errorJson: Message = WS.Json({ message: error, target: Message.instance });
 console.log(errorJson);
-const errorData: WSError = WS.Json({ message: errorJson.data, target: WS.WSError.instance });
+const errorData: WSError = WS.Json({ message: errorJson.data, target: WSError.instance });
 console.log(errorData);
 console.log('---------------------------------------------------');
--- client/data/src/protocole/ws-client.ts
+++ client/data/src/protocole/ws-client.ts
@@ -1,21 +1,204 @@
-import Message from './shared/Message.js';
-import WSError from './shared/WSError.js';
-
-import Pooler from './req/Pool.js';
-import Frame from './req/Frame.js';
-import Start from './req/Start.js';
-import Score from './req/Score.js';
-import Stop from './req/Stop.js';
-import Lost from './req/Lost.js';
-import Pool from './req/Pool.js';
-import Won from './req/Won.js';
-
-import Connect from './res/Connect.js';
-import Invite from './res/Invite.js';
-import Hook from './res/Hook.js';
-import Play from './res/Play.js';
 
 import _ from 'lodash';
+
+// ! shared -----------------------------------------------------------------------------
+
+interface MessageProps
+{
+	event: string;
+	object: Object;
+}
+
+export class Message
+{
+	public message: string;
+	public data: string;
+	constructor({ event, object }: MessageProps)
+	{
+		this.message = event;
+		this.data = JSON.stringify(object);
+	}
+	public static instance = new Message({event: '', object: {}});
+}
+
+export class WSError
+{
+	public message: string;
+	constructor(error: string)
+	{
+		this.message = error;
+	}
+	static instance = new WSError('');
+}
+
+export class User
+{
+	public username: string;
+	public email: string;
+	constructor(username: string, email: string)
+	{
+		this.username = username;
+		this.email = email;
+	}
+	static instance = new User('', '');
+}
+
+// ! res --------------------------------------------------------------------------------
+
+export class Connect
+{
+	// TODO: initial game data can be added here
+	username: string;
+	constructor(username: string)
+	{
+		this.username = username;
+	}
+	public static instance = new Connect('');
+}
+
+export class Hook
+{
+	up: boolean;
+	down: boolean;
+	constructor(up: boolean, down: boolean)
+	{
+		this.up = up;
+		this.down = down;
+	}
+	public static instance = new Hook(false, false);
+}
+
+export class Invite
+{
+	from: string;
+	to: string;
+	constructor(from: string, to: string)
+	{
+		this.from = from;
+		this.to = to;
+	}
+	public static instance = new Invite('', '');
+}
+
+export class Play
+{
+	username: string;
+	opponent: string;
+	constructor(username: string, opponent: string)
+	{
+		this.username = username;
+		this.opponent = opponent;
+	}
+	public static instance = new Play('', '');
+}
+
+// ! req --------------------------------------------------------------------------------
+
+export class Frame
+{
+	ballX: number = 0;
+	ballY: number = 0;
+	ballRadius: number = 0;
+	paddleRadius: number = 0;
+	leftPaddleTopX: number = 0;
+	leftPaddleTopY: number = 0;
+	rightPaddleTopX: number = 0;
+	rightPaddleTopY: number = 0;
+	leftPaddleBottomX: number = 0;
+	leftPaddleBottomY: number = 0;
+	rightPaddleBottomX: number = 0;
+	rightPaddleBottomY: number = 0;
+	constructor() {}
+	public static instance = new Frame();
+}
+
+export class Invitations
+{
+	public invitations: User[];
+	constructor(invitations: User[])
+	{
+		this.invitations = invitations;
+	}
+	static instance = new Invitations([]);
+}
+
+export class Lost
+{
+	public lost: string;
+	constructor()
+	{
+		this.lost = 'lost';
+	}
+	static instance = new Lost();
+}
+
+export class Pooler
+{
+	public username: string;
+	public profile: string;
+	constructor(username: string, profile: string)
+	{
+		this.username = username;
+		this.profile = profile;
+	}
+	static instance = new Pooler('', '');
+}
+
+export class Pool
+{
+	public pool: Pooler[];
+	constructor(pool: Pooler[])
+	{
+		this.pool = pool;
+	}
+	static instance = new Pool([]);
+}
+
+export class Score
+{
+	public player: number;
+	public opponent: number;
+	constructor(player: number, opponent: number)
+	{
+		this.player = player;
+		this.opponent = opponent;
+	}
+	static instance = new Score(0, 0);
+}
+
+export class Start
+{
+	public start: string;
+	constructor()
+	{
+		this.start = 'start';
+	}
+	public static instance = new Start();
+}
+
+export class Stop
+{
+	public stop: string;
+	constructor()
+	{
+		this.stop = 'stop';
+	}
+	public static instance = new Stop();
+}
+
+
+export class Won
+{
+	public won: string;
+	constructor()
+	{
+		this.won = 'won';
+	}
+	static instance = new Won();
+}
+
+
+// ! Protocole ------------------------------------------------------------
 
 interface JsonProps {
 	message: string;
@@ -30,24 +213,6 @@ class WS {
 	}
 
 	// * connect, invite, play, hook
-
-	// ? Protocole Classes
-	Message = Message;
-	WSError = WSError;
-
-	Pooler = Pooler;
-	Frame = Frame;
-	Start = Start;
-	Score = Score;
-	Stop = Stop;
-	Pool = Pool;
-	Lost = Lost;
-	Won = Won;
-
-	Connect = Connect;
-	Invite = Invite;
-	Play = Play;
-	Hook = Hook;
 
 	// ? Comminication Helpers
 	Json({ message, target }: JsonProps) {
